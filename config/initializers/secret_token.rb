282317e95514e7839e1d8771a83c4c35594866d1
David Huang <david@skyweavers.com>
2010-09-06 17:47:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citytreasure::Application.config.secret_token = 'f375c441b0bc9ac0c61654493a55410c0c93d04fab4e65adea5fca2ffd66af6b225bbd0a095ac7be647d1991cc1211dbefd4d3541a2e24c8406ab33cdb874d4d'
